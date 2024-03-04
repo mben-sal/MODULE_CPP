9391cb9e040e84e22121ec5d490e7f893e2524e9
Manar Ben salah <mben-sal@e2r7p1.1337.ma>
2024-03-04 13:38:54 +0100

    ex01

---
+++ MODULE_06/ex00/Conversion.cpp
@@ -0,0 +1,206 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Conversion.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 10:42:22 by mben-sal          #+#    #+#             */
+/*   Updated: 2024/03/04 13:34:20 by mben-sal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Conversion.hpp"
+
+Conversion::Conversion()
+{
+	
+}
+
+Conversion::Conversion(std::string str)
+{
+	this->str = str;
+	this->print = 1;
+	this->getType();
+}
+
+Conversion::Conversion(Conversion const &str)
+{
+	*this = str;
+}
+
+Conversion::~Conversion()
+{
+	
+}
+
+
+Conversion &Conversion::operator=(Conversion const &a)
+{
+    this->_char = a._char;
+    this->_double = a._double;
+    this->_Float = a._Float;
+    this->type = a.type;
+    this->print = a.print;
+    this->str = a.str;
+
+    return *this;
+}
+
+void Conversion::getType()
+{
+	if(!check_char() && !check_double() && !check_float() && !check_int())
+	{
+		std::cout << "arguments type unkown"<< std::endl;
+	}
+	this->cast();
+	this->print_type();
+}	
+
+bool Conversion::check_char()
+{
+	if(str.length() != 1)
+	{
+		return(0);
+	}
+	this->type = "char";
+	this->_char = str[0];
+	this->print = 1;
+	return(1);
+}
+
+bool Conversion::check_int()
+{
+	size_t i ;
+	try
+	{
+		this->_int = stoi(str, &i);
+		if( i != this->str.length())
+			return (0);
+		this->type = "int";
+		return(1);
+	}
+	catch(const std::exception& e)
+	{
+		print = 0;
+		return(0);
+	}
+}
+
+bool Conversion::check_float()
+{
+	size_t i ;
+	try
+	{
+		this->_Float = stof(str, &i);
+		if (this->str != "-inff" && this->str != "+inff"  && this->str != "nanf")
+			if (i != str.length() - 1 || str.find('.') == std::string::npos || str.c_str()[i] != 'f')
+				return (0);
+		this->type = "float";
+		return(1);
+	}
+	catch(const std::exception& e)
+	{
+		print = 0;
+		return(0);
+	}
+}
+
+bool	Conversion::check_double() {
+	size_t	i;
+
+	try {
+		this->_double = stod(str, &i);
+		if (this->str != "-inf" && this->str != "+inf"  && this->str != "nan")
+			if (i != str.length() || str.find('.') == std::string::npos || str.c_str()[i] != '\0')
+				return (false);
+		this->type = "double";
+		return (1);
+	}
+	catch (const std::exception &e) {
+		return (0);
+	}
+}
+void Conversion::cast()
+{
+	if(this->type == "char")
+		to_char();
+	else if(this->type == "int")
+		to_int();
+	else if(this->type == "float")
+		to_float();
+	else if(this->type == "double")
+		to_double();
+}
+
+void Conversion::to_char()
+{
+	this->_int = static_cast<int>(this->_char);
+	this->_Float = static_cast<float>(this->_char);
+	this->_double = static_cast<double>(this->_char);
+}
+
+void Conversion::to_double()
+{
+	this->_int = static_cast<int>(this->_double);
+	this->_char = static_cast<char>(this->_double);
+	this->_Float = static_cast<float>(this->_double);
+}
+
+void Conversion::to_float()
+{
+	if(print)
+	{
+		this->_int = static_cast<int>(this->_Float);
+		this->_char = static_cast<char>(this->_Float);
+	}
+	this->_double = static_cast<float>(this->_Float);
+}
+
+void Conversion::to_int()
+{
+	if(print)
+	{
+		this->_char = static_cast<char>(this->_int);
+		this->_double = static_cast<float>(this->_int);
+	}
+	this->_Float = static_cast<int>(this->_int);
+}
+
+void Conversion::print_type()
+{
+	this->print_char();
+	this->print_double();
+	this->print_float();
+	this->print_int();
+}
+
+void Conversion::print_char()
+{
+	if(!print || this->_int < -128 || this->_int > 127)
+		std::cout<< "char : impossible "<< std::endl;
+	else if(this->_int > 126 || this->_int < 32 )
+		std::cout << "char : non displayable "<< std::endl;
+	else
+		std::cout << "char" << std::endl;
+}
+
+void Conversion::print_double()
+{
+	std::cout<< "double" <<std::endl;
+}
+
+void Conversion::print_float()
+{
+	std::cout << "float" << std::endl;
+}
+
+void Conversion::print_int()
+{
+	if(!print)
+	{
+		std::cout<<" int impossible"<< std::endl;
+	}
+	else
+		std::cout << "int :" << std::endl;
+}
--- MODULE_06/ex01/main.cpp
+++ MODULE_06/ex01/main.cpp
@@ -5,19 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/04 12:57:21 by mben-sal          #+#    #+#             */
-/*   Updated: 2024/03/04 13:36:40 by mben-sal         ###   ########.fr       */
+/*   Created: 2024/03/04 13:20:59 by mben-sal          #+#    #+#             */
+/*   Updated: 2024/03/04 13:21:00 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"Conversion.hpp"
-
-int main(int ac, char **av)
-{
-	(void)av;
-	if(ac == 2)
-		Conversion N[1];
-	else 
-		std::cout<< "entre votre argument int ou char ..." << std::endl;
-	return (0);
-}
